d2de189b0098094fda67c3adf5198313d1bc6ade
Francois Manlay <francois.manlay@gmail.com>
2022-11-20 12:22:38 +0100

    Added validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   // property that will hold the form
   signupForm: FormGroup;
+  forbiddenUsername = ['Chris', 'Anna'];
+
+  ngOnInit() {
+    // initialize form with controls
+    // difference with this approach is that the form already exists and is accessible inside the component... no need to pass it from html template to componente, that's the beauty
+    this.signupForm = new FormGroup({
+      // form group inside form group (nested)
+      userData: new FormGroup({
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
+      }),
+      gender: new FormControl('male'),
+      hobbies: new FormArray([]),
+    });
+  }
+
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  addHobbies() {
+    // we need to explicitely cast this, using this syntax, to make it clear we push inside the formarray
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // customer validator function
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsername.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
+  }
+
+  // asynchronous validator
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 700);
+    });
+    return promise;
+  }
 }
