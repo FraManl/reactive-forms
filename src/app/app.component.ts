8210bd9e9e2e2094edce74bbc08559a2300d6bb6
Francois Manlay <francois.manlay@gmail.com>
2022-11-20 10:21:55 +0100

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  genders = ['male', 'female'];
+  // property that will hold the form
+  signupForm: FormGroup;
+}
